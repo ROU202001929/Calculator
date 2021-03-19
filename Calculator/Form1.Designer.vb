ed89cc200c39842897927999329149edf4884e1d
Lucas Routley <ROU202001929@longroad.ac.uk>
2021-03-19 14:18:24 +0000

    Commit

---
+++ Calculator/Form1.Designer.vb
@@ -0,0 +1,277 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CmdON = New System.Windows.Forms.Button()
+        Me.TxtDisplay = New System.Windows.Forms.TextBox()
+        Me.CmdDEL = New System.Windows.Forms.Button()
+        Me.CmdThree = New System.Windows.Forms.Button()
+        Me.CmdTwo = New System.Windows.Forms.Button()
+        Me.CmdOne = New System.Windows.Forms.Button()
+        Me.CmdFour = New System.Windows.Forms.Button()
+        Me.CmdFive = New System.Windows.Forms.Button()
+        Me.CmdSix = New System.Windows.Forms.Button()
+        Me.Cmd7 = New System.Windows.Forms.Button()
+        Me.CmdEight = New System.Windows.Forms.Button()
+        Me.CmdNine = New System.Windows.Forms.Button()
+        Me.CmdAC = New System.Windows.Forms.Button()
+        Me.CmdZero = New System.Windows.Forms.Button()
+        Me.CmdMultiply = New System.Windows.Forms.Button()
+        Me.CmdDivide = New System.Windows.Forms.Button()
+        Me.CmdPlus = New System.Windows.Forms.Button()
+        Me.CmdMinus = New System.Windows.Forms.Button()
+        Me.CmdANS = New System.Windows.Forms.Button()
+        Me.CmdEquals = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'CmdON
+        '
+        Me.CmdON.Location = New System.Drawing.Point(358, 38)
+        Me.CmdON.Name = "CmdON"
+        Me.CmdON.Size = New System.Drawing.Size(75, 23)
+        Me.CmdON.TabIndex = 0
+        Me.CmdON.Text = "ON/OFF"
+        Me.CmdON.UseVisualStyleBackColor = True
+        '
+        'TxtDisplay
+        '
+        Me.TxtDisplay.Location = New System.Drawing.Point(12, 12)
+        Me.TxtDisplay.Name = "TxtDisplay"
+        Me.TxtDisplay.Size = New System.Drawing.Size(421, 20)
+        Me.TxtDisplay.TabIndex = 1
+        '
+        'CmdDEL
+        '
+        Me.CmdDEL.Location = New System.Drawing.Point(277, 67)
+        Me.CmdDEL.Name = "CmdDEL"
+        Me.CmdDEL.Size = New System.Drawing.Size(75, 23)
+        Me.CmdDEL.TabIndex = 2
+        Me.CmdDEL.Text = "DEL"
+        Me.CmdDEL.UseVisualStyleBackColor = True
+        '
+        'CmdThree
+        '
+        Me.CmdThree.Location = New System.Drawing.Point(174, 67)
+        Me.CmdThree.Name = "CmdThree"
+        Me.CmdThree.Size = New System.Drawing.Size(75, 23)
+        Me.CmdThree.TabIndex = 3
+        Me.CmdThree.Text = "3"
+        Me.CmdThree.UseVisualStyleBackColor = True
+        '
+        'CmdTwo
+        '
+        Me.CmdTwo.Location = New System.Drawing.Point(93, 67)
+        Me.CmdTwo.Name = "CmdTwo"
+        Me.CmdTwo.Size = New System.Drawing.Size(75, 23)
+        Me.CmdTwo.TabIndex = 4
+        Me.CmdTwo.Text = "2"
+        Me.CmdTwo.UseVisualStyleBackColor = True
+        '
+        'CmdOne
+        '
+        Me.CmdOne.Location = New System.Drawing.Point(12, 67)
+        Me.CmdOne.Name = "CmdOne"
+        Me.CmdOne.Size = New System.Drawing.Size(75, 23)
+        Me.CmdOne.TabIndex = 5
+        Me.CmdOne.Text = "1"
+        Me.CmdOne.UseVisualStyleBackColor = True
+        '
+        'CmdFour
+        '
+        Me.CmdFour.Location = New System.Drawing.Point(12, 96)
+        Me.CmdFour.Name = "CmdFour"
+        Me.CmdFour.Size = New System.Drawing.Size(75, 23)
+        Me.CmdFour.TabIndex = 6
+        Me.CmdFour.Text = "4"
+        Me.CmdFour.UseVisualStyleBackColor = True
+        '
+        'CmdFive
+        '
+        Me.CmdFive.Location = New System.Drawing.Point(93, 96)
+        Me.CmdFive.Name = "CmdFive"
+        Me.CmdFive.Size = New System.Drawing.Size(75, 23)
+        Me.CmdFive.TabIndex = 7
+        Me.CmdFive.Text = "5"
+        Me.CmdFive.UseVisualStyleBackColor = True
+        '
+        'CmdSix
+        '
+        Me.CmdSix.Location = New System.Drawing.Point(174, 96)
+        Me.CmdSix.Name = "CmdSix"
+        Me.CmdSix.Size = New System.Drawing.Size(75, 23)
+        Me.CmdSix.TabIndex = 8
+        Me.CmdSix.Text = "6"
+        Me.CmdSix.UseVisualStyleBackColor = True
+        '
+        'Cmd7
+        '
+        Me.Cmd7.Location = New System.Drawing.Point(12, 125)
+        Me.Cmd7.Name = "Cmd7"
+        Me.Cmd7.Size = New System.Drawing.Size(75, 23)
+        Me.Cmd7.TabIndex = 9
+        Me.Cmd7.Text = "7"
+        Me.Cmd7.UseVisualStyleBackColor = True
+        '
+        'CmdEight
+        '
+        Me.CmdEight.Location = New System.Drawing.Point(93, 125)
+        Me.CmdEight.Name = "CmdEight"
+        Me.CmdEight.Size = New System.Drawing.Size(75, 23)
+        Me.CmdEight.TabIndex = 10
+        Me.CmdEight.Text = "8"
+        Me.CmdEight.UseVisualStyleBackColor = True
+        '
+        'CmdNine
+        '
+        Me.CmdNine.Location = New System.Drawing.Point(174, 125)
+        Me.CmdNine.Name = "CmdNine"
+        Me.CmdNine.Size = New System.Drawing.Size(75, 23)
+        Me.CmdNine.TabIndex = 11
+        Me.CmdNine.Text = "9"
+        Me.CmdNine.UseVisualStyleBackColor = True
+        '
+        'CmdAC
+        '
+        Me.CmdAC.Location = New System.Drawing.Point(358, 67)
+        Me.CmdAC.Name = "CmdAC"
+        Me.CmdAC.Size = New System.Drawing.Size(75, 23)
+        Me.CmdAC.TabIndex = 12
+        Me.CmdAC.Text = "AC"
+        Me.CmdAC.UseVisualStyleBackColor = True
+        '
+        'CmdZero
+        '
+        Me.CmdZero.Location = New System.Drawing.Point(93, 154)
+        Me.CmdZero.Name = "CmdZero"
+        Me.CmdZero.Size = New System.Drawing.Size(75, 23)
+        Me.CmdZero.TabIndex = 13
+        Me.CmdZero.Text = "0"
+        Me.CmdZero.UseVisualStyleBackColor = True
+        '
+        'CmdMultiply
+        '
+        Me.CmdMultiply.Location = New System.Drawing.Point(277, 96)
+        Me.CmdMultiply.Name = "CmdMultiply"
+        Me.CmdMultiply.Size = New System.Drawing.Size(75, 23)
+        Me.CmdMultiply.TabIndex = 14
+        Me.CmdMultiply.Text = "x"
+        Me.CmdMultiply.UseVisualStyleBackColor = True
+        '
+        'CmdDivide
+        '
+        Me.CmdDivide.Location = New System.Drawing.Point(358, 96)
+        Me.CmdDivide.Name = "CmdDivide"
+        Me.CmdDivide.Size = New System.Drawing.Size(75, 23)
+        Me.CmdDivide.TabIndex = 15
+        Me.CmdDivide.Text = "÷"
+        Me.CmdDivide.UseVisualStyleBackColor = True
+        '
+        'CmdPlus
+        '
+        Me.CmdPlus.Location = New System.Drawing.Point(277, 125)
+        Me.CmdPlus.Name = "CmdPlus"
+        Me.CmdPlus.Size = New System.Drawing.Size(75, 23)
+        Me.CmdPlus.TabIndex = 16
+        Me.CmdPlus.Text = "+"
+        Me.CmdPlus.UseVisualStyleBackColor = True
+        '
+        'CmdMinus
+        '
+        Me.CmdMinus.Location = New System.Drawing.Point(358, 125)
+        Me.CmdMinus.Name = "CmdMinus"
+        Me.CmdMinus.Size = New System.Drawing.Size(75, 23)
+        Me.CmdMinus.TabIndex = 17
+        Me.CmdMinus.Text = "-"
+        Me.CmdMinus.UseVisualStyleBackColor = True
+        '
+        'CmdANS
+        '
+        Me.CmdANS.Location = New System.Drawing.Point(277, 154)
+        Me.CmdANS.Name = "CmdANS"
+        Me.CmdANS.Size = New System.Drawing.Size(75, 23)
+        Me.CmdANS.TabIndex = 18
+        Me.CmdANS.Text = "Ans"
+        Me.CmdANS.UseVisualStyleBackColor = True
+        '
+        'CmdEquals
+        '
+        Me.CmdEquals.Location = New System.Drawing.Point(358, 154)
+        Me.CmdEquals.Name = "CmdEquals"
+        Me.CmdEquals.Size = New System.Drawing.Size(75, 23)
+        Me.CmdEquals.TabIndex = 19
+        Me.CmdEquals.Text = "="
+        Me.CmdEquals.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(445, 489)
+        Me.Controls.Add(Me.CmdEquals)
+        Me.Controls.Add(Me.CmdANS)
+        Me.Controls.Add(Me.CmdMinus)
+        Me.Controls.Add(Me.CmdPlus)
+        Me.Controls.Add(Me.CmdDivide)
+        Me.Controls.Add(Me.CmdMultiply)
+        Me.Controls.Add(Me.CmdZero)
+        Me.Controls.Add(Me.CmdAC)
+        Me.Controls.Add(Me.CmdNine)
+        Me.Controls.Add(Me.CmdEight)
+        Me.Controls.Add(Me.Cmd7)
+        Me.Controls.Add(Me.CmdSix)
+        Me.Controls.Add(Me.CmdFive)
+        Me.Controls.Add(Me.CmdFour)
+        Me.Controls.Add(Me.CmdOne)
+        Me.Controls.Add(Me.CmdTwo)
+        Me.Controls.Add(Me.CmdThree)
+        Me.Controls.Add(Me.CmdDEL)
+        Me.Controls.Add(Me.TxtDisplay)
+        Me.Controls.Add(Me.CmdON)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CmdON As Button
+    Friend WithEvents TxtDisplay As TextBox
+    Friend WithEvents CmdDEL As Button
+    Friend WithEvents CmdThree As Button
+    Friend WithEvents CmdTwo As Button
+    Friend WithEvents CmdOne As Button
+    Friend WithEvents CmdFour As Button
+    Friend WithEvents CmdFive As Button
+    Friend WithEvents CmdSix As Button
+    Friend WithEvents Cmd7 As Button
+    Friend WithEvents CmdEight As Button
+    Friend WithEvents CmdNine As Button
+    Friend WithEvents CmdAC As Button
+    Friend WithEvents CmdZero As Button
+    Friend WithEvents CmdMultiply As Button
+    Friend WithEvents CmdDivide As Button
+    Friend WithEvents CmdPlus As Button
+    Friend WithEvents CmdMinus As Button
+    Friend WithEvents CmdANS As Button
+    Friend WithEvents CmdEquals As Button
+End Class
---
+++ Calculator/Form1.vb
@@ -0,0 +1,103 @@
+﻿Public Class Form1
+    Dim Operand1 As Int64
+    Dim Operand2 As Int64
+    Dim op As String
+    Dim Ans As Int64
+    Private Sub CmdON_Click(sender As Object, e As EventArgs) Handles CmdON.Click
+
+    End Sub
+
+    Private Sub CmdOne_Click(sender As Object, e As EventArgs) Handles CmdOne.Click
+        TxtDisplay.AppendText(1)
+    End Sub
+
+    Private Sub CmdTwo_Click(sender As Object, e As EventArgs) Handles CmdTwo.Click
+        TxtDisplay.AppendText(2)
+    End Sub
+
+    Private Sub CmdThree_Click(sender As Object, e As EventArgs) Handles CmdThree.Click
+        TxtDisplay.AppendText(3)
+    End Sub
+
+    Private Sub CmdFour_Click(sender As Object, e As EventArgs) Handles CmdFour.Click
+        TxtDisplay.AppendText(4)
+    End Sub
+
+    Private Sub CmdFive_Click(sender As Object, e As EventArgs) Handles CmdFive.Click
+        TxtDisplay.AppendText(5)
+    End Sub
+
+    Private Sub CmdSix_Click(sender As Object, e As EventArgs) Handles CmdSix.Click
+        TxtDisplay.AppendText(6)
+    End Sub
+
+    Private Sub Cmd7_Click(sender As Object, e As EventArgs) Handles Cmd7.Click
+        TxtDisplay.AppendText(7)
+    End Sub
+
+    Private Sub CmdEight_Click(sender As Object, e As EventArgs) Handles CmdEight.Click
+        TxtDisplay.AppendText(8)
+    End Sub
+
+    Private Sub CmdNine_Click(sender As Object, e As EventArgs) Handles CmdNine.Click
+        TxtDisplay.AppendText(9)
+    End Sub
+
+    Private Sub CmdZero_Click(sender As Object, e As EventArgs) Handles CmdZero.Click
+        TxtDisplay.AppendText(0)
+    End Sub
+
+    Private Sub CmdDEL_Click(sender As Object, e As EventArgs) Handles CmdDEL.Click
+
+    End Sub
+
+    Private Sub CmdAC_Click(sender As Object, e As EventArgs) Handles CmdAC.Click
+        TxtDisplay.Text = ""
+    End Sub
+
+    Private Sub CmdMultiply_Click(sender As Object, e As EventArgs) Handles CmdMultiply.Click
+        Operand1 = TxtDisplay.Text
+        op = "x"
+        TxtDisplay.Text = ""
+    End Sub
+
+    Private Sub CmdDivide_Click(sender As Object, e As EventArgs) Handles CmdDivide.Click
+        Operand1 = TxtDisplay.Text
+        op = "/"
+        TxtDisplay.Text = ""
+    End Sub
+
+    Private Sub CmdPlus_Click(sender As Object, e As EventArgs) Handles CmdPlus.Click
+        Operand1 = TxtDisplay.Text
+        op = "+"
+        TxtDisplay.Text = ""
+    End Sub
+
+    Private Sub CmdMinus_Click(sender As Object, e As EventArgs) Handles CmdMinus.Click
+        Operand1 = TxtDisplay.Text
+        op = "-"
+        TxtDisplay.Text = ""
+    End Sub
+
+    Private Sub CmdANS_Click(sender As Object, e As EventArgs) Handles CmdANS.Click
+        TxtDisplay.AppendText(Ans)
+    End Sub
+
+    Private Sub CmdEquals_Click(sender As Object, e As EventArgs) Handles CmdEquals.Click
+        Operand2 = TxtDisplay.Text
+        If op = "x" Then
+            Ans = Operand1 * Operand2
+        ElseIf op = "/" Then
+            Ans = Operand1 / Operand2
+        ElseIf op = "+" Then
+            Ans = Operand1 + Operand2
+        ElseIf op = "-" Then
+            Ans = Operand1 - Operand2
+        End If
+        TxtDisplay.Text = Ans
+    End Sub
+
+    Private Sub TxtDisplay_TextChanged(sender As Object, e As EventArgs) Handles TxtDisplay.TextChanged
+
+    End Sub
+End Class
